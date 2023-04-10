8df424dc7186ee6fd07cb2665fb5857a3167b4a4
Aiden Appleby <aiden.appleby3@gmail.com>
2023-04-10 10:45:05 -0400

    fixed redirector

--- pages/team/dashboard/index.tsx
+++ pages/team/dashboard/index.tsx
@@ -1,6 +1,6 @@
 export default function Dashboard() {}
 
-export const getStaticProps = () => {
+export const getServerSideProps = () => {
 	return {
 		redirect: {
             destination: '/team/dashboard/assets',
