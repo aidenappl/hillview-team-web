fed526e94b3f2f71ff1b05d681f3746685891f9a
Aiden Appleby <aiden.appleby3@gmail.com>
2023-04-12 21:10:20 -0400

    added inspector

--- pages/team/dashboard/playlists.tsx
+++ pages/team/dashboard/playlists.tsx
@@ -7,10 +7,20 @@ import { useEffect, useState } from "react";
 import { Playlist } from "../../../models/playlist.model";
 import Image from "next/image";
 import { NewRequest } from "../../../services/http/requestHandler";
+import TeamModal from "../../../components/pages/team/TeamModal";
+import TeamModalInput from "../../../components/pages/team/TeamModalInput";
+import TeamModalSelect from "../../../components/pages/team/TeamModalSelect";
+import TeamModalTextarea from "../../../components/pages/team/TeamModalTextarea";
+import { VideoStatuses } from "../../../models/videoStatus.model";
+import toast from "react-hot-toast";
 
 const PlaylistsPage = () => {
 	const router = useRouter();
 	const [playlists, setPlaylists] = useState<Playlist[] | null>(null);
+
+	// Video Inspector
+	const [saving, setSaving] = useState<boolean>(false);
+	const [changes, setChanges] = useState<any>(null);
 	const [selectedPlaylist, setSelectedPlaylist] = useState<Playlist | null>(
 		null
 	);
@@ -33,12 +43,130 @@ const PlaylistsPage = () => {
 		}
 	};
 
+	const inputChange = async (modifier: Object) => {
+		setChanges({ ...changes, ...modifier });
+	};
+
+	const deleteChange = async (key: string, forcedArr?: any) => {
+		let splitKey = key.split(".");
+		let newChanges;
+		if (forcedArr) {
+			newChanges = { ...forcedArr };
+		} else {
+			newChanges = { ...changes };
+		}
+		for (var k in newChanges) {
+			if (k == key) {
+				delete newChanges[key];
+				setChanges(newChanges);
+			} else if (typeof newChanges[k] === "object" && splitKey[0] == k) {
+				deleteChange(splitKey[1], newChanges[k]);
+			}
+		}
+	};
+
+	const cancelPlaylistInspection = async () => {
+		setSelectedPlaylist(null);
+		setChanges(null);
+		setSaving(false);
+	};
+
+	const savePlaylistInspection = async () => {
+		if (changes && Object.keys(changes).length > 0) {
+			setSaving(true);
+			const response = await NewRequest({
+				method: "PUT",
+				route: "/core/v1.1/admin/playlist/" + selectedPlaylist!.id,
+				body: {
+					id: selectedPlaylist!.id,
+					changes: changes,
+				},
+				auth: true,
+			});
+			if (response.success) {
+				setSelectedPlaylist(null);
+				setChanges(null);
+				setSaving(false);
+				initialize();
+			} else {
+				console.error(response);
+				setSaving(false);
+				toast.error("Failed to save changes", {
+					position: "top-center",
+				});
+			}
+		} else {
+			setSelectedPlaylist(null);
+		}
+	};
+
 	useEffect(() => {
 		initialize();
 	}, []);
 
 	return (
 		<TeamContainer pageTitle="Playlists" router={router}>
+			{selectedPlaylist ? (
+				<TeamModal
+					className="gap-6"
+					loader={saving}
+					saveActive={changes && Object.keys(changes).length > 0}
+					cancelHit={() => cancelPlaylistInspection()}
+					saveHit={() => savePlaylistInspection()}
+					deleteHit={() => setShowConfirmDeleteVideo(true)}
+					destructiveText="Archive"
+				>
+					<TeamModalInput
+						title="Title"
+						placeholder="Playlist Title"
+						value={selectedPlaylist.name}
+						setValue={(value: string) => {
+							if (value != selectedPlaylist.name) {
+								inputChange({ name: value });
+							} else {
+								deleteChange("name");
+							}
+						}}
+					/>
+					<TeamModalTextarea
+						title="Description"
+						placeholder="Playlist Description"
+						value={selectedPlaylist.description}
+						className="h-[150px]"
+						setValue={(value: string) => {
+							if (value != selectedPlaylist.description) {
+								inputChange({ description: value });
+							} else {
+								deleteChange("description");
+							}
+						}}
+					/>
+					<TeamModalInput
+						title="Route"
+						placeholder="Playlist Source Route"
+						value={selectedPlaylist.route}
+						setValue={(value: string) => {
+							if (value != selectedPlaylist.route) {
+								inputChange({ route: value });
+							} else {
+								deleteChange("route");
+							}
+						}}
+					/>
+					<TeamModalInput
+						title="Banner Image URL"
+						placeholder="Playlist Banner Image URL"
+						value={selectedPlaylist.banner_image}
+						setValue={(value: string) => {
+							if (value != selectedPlaylist.banner_image) {
+								inputChange({ banner_image: value });
+							} else {
+								deleteChange("banner_image");
+							}
+						}}
+					/>
+				</TeamModal>
+			) : null}
 			{/* Team Heading */}
 			<TeamHeader title="System Playlists" />
 			{/* Data Body */}
@@ -87,7 +215,7 @@ const PlaylistsPage = () => {
 										</div>
 										<p className="w-1/2">{playlist.name}</p>
 										<p className="w-1/2">
-											{playlist.route}
+											/{playlist.route}
 										</p>
 										<div className="w-[200px] flex gap-2 pr-10">
 											<button
