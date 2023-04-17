83269cdce2aece06c01040f0a1650c303008b986
Aiden Appleby <aiden.appleby3@gmail.com>
2023-04-17 01:17:15 -0400

    added under construction warning

--- pages/team/dashboard/users.tsx
+++ pages/team/dashboard/users.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router";
 import TeamContainer from "../../../components/pages/team/TeamContainer";
+import TeamHeader from "../../../components/pages/team/TeamHeader";
 
 const UsersPage = () => {
 	const router = useRouter();
 	return (
 		<TeamContainer pageTitle="Users" router={router}>
-			<h1>Users</h1>
+			<TeamHeader title={"Platform Users - Under Construction (sorry)"}/>
 		</TeamContainer>
 	);
 };
