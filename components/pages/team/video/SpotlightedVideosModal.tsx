b67a64635767d7356514497216d2fda735b26c83
Aiden Appleby <aiden.appleby3@gmail.com>
2025-02-14 17:02:03 -0500

    Update SpotlightedVideosModal.tsx

--- components/pages/team/video/SpotlightedVideosModal.tsx
+++ components/pages/team/video/SpotlightedVideosModal.tsx
@@ -39,18 +39,39 @@ const SpotlightedVideosModal = (props: Props) => {
 				}}
 			>
 				<div className="flex flex-col gap-2">
-					<div className="flex flex-col gap-1">
-						<h2 className="text-[#101827] font-semibold text-sm">
+					<div className="flex flex-col gap-4">
+						<h2 className="text-[#101827] font-medium text-xl">
 							Spotlighted Videos
 						</h2>
-						{spotlightedVideos != null &&
-						spotlightedVideos.length > 0 ? (
-							spotlightedVideos.map((spot: any) => (
-								<div key={spot.rank}>#{spot.rank} - {spot.video.title}</div>
-							))
-						) : (
-							<option value={""}>No Videos</option>
-						)}
+						<div className="flex flex-col gap-2">
+							{spotlightedVideos != null &&
+							spotlightedVideos.length > 0 ? (
+								spotlightedVideos.map((spot: any) => (
+									<div
+										key={spot.rank}
+										className="flex items-center gap-1"
+									>
+										<p className="font-semibold">
+											#{spot.rank} -{" "}
+										</p>
+										{spot.video ? (
+											<div className="flex gap-2 items-center">
+												{spot.video.title}
+												<button className="px-2 text-xs py-1 bg-slate-500 hover:bg-slate-700 transition text-white rounded-md font-medium">
+													Change Video
+												</button>
+											</div>
+										) : (
+											<button className="px-2 text-xs py-1 bg-slate-500 hover:bg-slate-700 transition text-white rounded-md font-medium">
+												Add Video
+											</button>
+										)}
+									</div>
+								))
+							) : (
+								<option value={""}>No Videos</option>
+							)}
+						</div>
 					</div>
 				</div>
 			</TeamModal>
