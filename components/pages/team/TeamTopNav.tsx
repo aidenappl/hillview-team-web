74e41d3a942120dfacca9d5d9b340feac6d4d6b6
Aiden Appleby <aiden.appleby3@gmail.com>
2025-08-13 22:32:47 -0400

    Update TeamTopNav.tsx

--- components/pages/team/TeamTopNav.tsx
+++ components/pages/team/TeamTopNav.tsx
@@ -1,4 +1,8 @@
 import dayjs from "dayjs";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../../redux/user/slice";
+import { User } from "../../../types";
+import Image from "next/image";
 require("dayjs/locale/en");
 
 interface Props {
@@ -8,6 +12,7 @@ interface Props {
 const TeamTopNav = (props: Props) => {
 	const date = dayjs();
 	const formattedDate = date.locale("en").format("dddd MMMM DD, YYYY");
+	const user: User = useSelector(selectUser);
 
 	return (
 		<div className="w-full h-[100px] md:h-[160px] relative flex items-center justify-between">
@@ -17,6 +22,16 @@ const TeamTopNav = (props: Props) => {
 				<p className="text-[#8792c1] font-light hidden md:block">
 					{formattedDate}
 				</p>
+			</div>
+			{/* Right Content */}
+			<div className="block md:hidden w-[40px] h-[40px] bg-slate-100 shadow-sm border border-slate-5 rounded-full">
+				<Image
+					src={user?.profile_image_url || "/default-profile.png"}
+					alt="Profile Picture"
+					width={40}
+					height={40}
+					className="rounded-full"
+				/>
 			</div>
 			{/* Hr */}
 			<div className="h-[1px] w-full absolute bottom-0 right-0 bg-[#ebf0f6]" />
