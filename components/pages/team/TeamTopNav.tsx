362b27b49cead0a0443e97c820c6082966df6829
Aiden Appleby <aiden.appleby3@gmail.com>
2025-08-13 22:12:21 -0400

    work on responsiveness

--- components/pages/team/TeamTopNav.tsx
+++ components/pages/team/TeamTopNav.tsx
@@ -10,13 +10,13 @@ const TeamTopNav = (props: Props) => {
 	const formattedDate = date.locale("en").format("dddd MMMM DD, YYYY");
 
 	return (
-		<div className="w-full h-[160px] relative flex items-center justify-between">
+		<div className="w-full h-[100px] md:h-[160px] relative flex items-center justify-between">
 			{/* Left Content & Page Title */}
-			<div className="flex flex-col gap-1">
-				<h1 className="text-3xl font-bold text-[#21304f]">
-					{props.pageTitle}
-				</h1>
-				<p className="text-[#8792c1] font-light">{formattedDate}</p>
+			<div className="flex sm:flex-col gap-1">
+				<h1 className="text-3xl font-bold text-[#21304f]">{props.pageTitle}</h1>
+				<p className="text-[#8792c1] font-light hidden md:block">
+					{formattedDate}
+				</p>
 			</div>
 			{/* Hr */}
 			<div className="h-[1px] w-full absolute bottom-0 right-0 bg-[#ebf0f6]" />
